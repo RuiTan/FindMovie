87b78100a99bb7922d52556ab7ae157a475bdea7
Archer <tanrui106@163.com>
2018-10-08 21:04:59 +0800

    完成文件解析java代码

---
+++ 解析文本文件/SplitDataFile/src/com/tanrui/Main.java
@@ -0,0 +1,9 @@
+package com.tanrui;
+
+public class Main {
+    public static void main(String[] args){
+
+        SplitFile splitFile = new SplitFile();
+
+    }
+}
---
+++ 解析文本文件/SplitDataFile/src/com/tanrui/SplitFile.java
@@ -0,0 +1,100 @@
+package com.tanrui;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class SplitFile {
+
+    public static final String path = "";
+
+    public static final String fileName = "movie.txt";
+
+    public static final String defaultOutFile = "movie_out_";
+
+    public static final String fileType = ".txt";
+
+    private InputStream is;
+
+    private BufferedWriter os;
+
+    private Scanner sc;
+
+    public static final int DefaultLines = 1000;
+
+    private FileWriter CreateFile(String fn){
+
+        FileWriter file = null;
+        try {
+            file = new FileWriter(fn);
+        } catch (IOException e) {
+            System.out.println("SDF error : Cannot create file \'" + fn + "\'!");
+            e.printStackTrace();
+        }
+
+        return file;
+    }
+
+
+    public void ReadAndSplit(){
+
+        // 读取data
+        try {
+            is = new FileInputStream(path + fileName);
+        } catch (FileNotFoundException e) {
+            System.out.println("SDF error : File \'" + path + fileName + "\' not found!");
+            e.printStackTrace();
+        }
+        // 创建输入流Scanner
+        sc = new Scanner(is);
+
+        int flag = 1;
+        int mount = 0;
+
+        // 创建输出文件
+        String fn = path + defaultOutFile + flag + fileType;
+
+        FileWriter file = CreateFile(fn);
+
+        // 创建输出流
+        try {
+            os = new BufferedWriter(file);
+
+            while (sc.hasNextLine()){
+
+                mount++;
+
+                // 限定每个文件写DefaultLines行数的数据
+                if (mount == DefaultLines){
+                    os.close();
+                    ++flag;
+                    fn = path + defaultOutFile + flag + fileType;
+                    file = CreateFile(fn);
+                    mount = 0;
+                    os = new BufferedWriter(file);
+                    System.out.println("SDF log : file " + flag + " has been written!");
+                }
+
+                // 往文件里面写
+                String line = sc.nextLine();
+                try {
+                    os.write(line);
+                } catch (IOException e) {
+                    System.out.println("SDF error : Cannot write into file \'" + path + fileName + "\'!");
+                    e.printStackTrace();
+                }
+
+            }
+
+            os.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("SDF error : File \'" + path + fileName + "\' not found!");
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("SDF log : " + flag + " files!");
+
+    }
+}
